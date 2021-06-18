964b6533f012ffb56e56fa7faecebba01b11f267
Joe Becher <drazisil@users.noreply.github.com>
2021-06-17 20:30:47 -0400

    docs: rename and add docs

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,9 @@
-const { SigManager } = require('../lib')
+const { SigDB } = require('../lib')
 const tap = require('tap')
 
-const sigManager = new SigManager()
+const sigDB = new SigDB()
 
 tap.equal(
-  sigManager.find(Buffer.from([0x21, 0x3c, 0x61, 0x72, 0x63, 0x68, 0x3e])).name,
+  sigDB.find(Buffer.from([0x21, 0x3c, 0x61, 0x72, 0x63, 0x68, 0x3e])).name,
   'ar archive',
 )
